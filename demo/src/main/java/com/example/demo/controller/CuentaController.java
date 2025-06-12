a12a720b7d64455d38956a1eb22ebf7ddb83c66a
jonathan35356 <j.javier200211@outlook.com>
2025-06-12 18:05:16 -0500

    vista

--- demo/src/main/java/com/example/demo/controller/CuentaController.java
+++ demo/src/main/java/com/example/demo/controller/CuentaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Controller
 public class CuentaController {
